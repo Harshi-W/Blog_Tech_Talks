1505a06e31e52514c2553e2c884bda30a52964e7
harshi <harshipeiris2001@gmail.com>
2023-09-25 14:38:03 +0530

    Create Procfile.

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = 'FLASK_KEY'
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
